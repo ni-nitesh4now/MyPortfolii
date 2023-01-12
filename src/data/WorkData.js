00de758da779094943b932404edfaf8bc0ebd703
Nitesh Mishra <77237378+ni-nitesh4now@users.noreply.github.com>
2023-01-12 20:08:49 +0530

    Update WorkData.js

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -4,28 +4,28 @@ export const Work = [
     {
         id:1,
         name:"Project 1",
-        description:"Backend validates every entry by checking the IP Address of the user so that there would not be any attempt of proxy.",
+        description:"ATTENDANCE MARKER: Backend validates every entry by checking the IP Address of the user so that there would not be any attempt of proxy.",
         tags:["PHP", "MySQL", "phpMyAdmin", "JavaScript"],
         demo:"http://communication.epizy.com/"
     }
     ,{
         id:2,
         name:"Project 2",
-        description:"A GUI for the university management system to maintain the data of Staff, Exams, Students and Library books.",
+        description:"UNIVERSITY MANAGEMENT SYSTEM: A GUI for the university management system to maintain the data of Staff, Exams, Students and Library books.",
         tags:["Python","(Tkinter, Pillow)", "MySQL for Python"],
         demo:"https://drive.google.com/drive/folders/1iYcOqWcRbBzcvTreDJ9mfmC-Sz88JOfj?usp=share_link"
     }
     ,{
         id:3,
         name:"Project 3",
-        description:"Developed in React.js framework, these websites provide a unique user experience.",
+        description:"PORTFOLIO WEBSITE: Developed in React.js framework, these websites provide a unique user experience.",
         tags:["React.Js","Tailwind CSS", "JavaScript"],
         demo:"https://nitesh-mishra.vercel.app/"
     }
     ,{
         id:4,
         name:"Project 4",
-        description:"Pet-Vet: A blogging website for pet health care and guide.",
+        description:"PET-VET: A blogging website for pet health care and guide.",
         tags:["React.Js","Tailwind CSS", "JavaScript"],
         demo:"#"
     }
