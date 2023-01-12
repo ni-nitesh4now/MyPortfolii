3b3545574616a7614e8999adb9486ecc98747a88
Nitesh Mishra <77237378+ni-nitesh4now@users.noreply.github.com>
2023-01-12 19:40:38 +0530

    Update WorkData.js

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -4,16 +4,30 @@ export const Work = [
     {
         id:1,
         name:"Project 1",
-        description:"This project has a basic overview of spur gear, it's modelling and Structural Analysis. Software used for designing  - CATIA Software used for Analysis - Ansys Workbench",
-        tags:["Designing","Gear","CATIA"],
-        demo:"https:"
+        description:"Backend validates every entry by checking the IP Address of the user so that there would not be any attempt of proxy.",
+        tags:["PHP", "MySQL", "phpMyAdmin", "JavaScript"],
+        demo:"http://communication.epizy.com/"
     }
     ,{
         id:2,
         name:"Project 2",
-        description:"It consists of the Modelling and Analysis of Centrifugal Pump Impeller. Design of Impeller is done using Computer Aided Design software CATIA. It's Analysis is done using Ansys Workbench. Basically for two different materials.",
-        tags:["Impeller","Ansys"],
-        demo:"http//"
+        description:"A GUI for the university management system to maintain the data of Staff, Exams, Students and Library books.",
+        tags:["Python","(Tkinter, Pillow)", "MySQL for Python"],
+        demo:"https://drive.google.com/drive/folders/1iYcOqWcRbBzcvTreDJ9mfmC-Sz88JOfj?usp=share_link"
+    }
+    ,{
+        id:3,
+        name:"Project 3",
+        description:"Developed in React.js framework, these websites provide a unique user experience.",
+        tags:["React.Js","Tailwind CSS", "JavaScript"],
+        demo:"https://nitesh-mishra.vercel.app/"
+    }
+    ,{
+        id:4,
+        name:"Project 4",
+        description:"Pet-Vet: A blogging website for pet health care and guide.",
+        tags:["React.Js","Tailwind CSS", "JavaScript"],
+        demo:"#"
     }
     
   
