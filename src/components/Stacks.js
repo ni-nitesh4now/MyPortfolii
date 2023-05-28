37c34a26f4fe277ea484e85b2a3026ad8c92aaa3
Nitesh Mishra <77237378+ni-nitesh4now@users.noreply.github.com>
2023-05-28 15:58:02 +0530

    Create Stacks.js

---
+++ src/components/Stacks.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+const Stacks = () => {
+  return (
+    <div className="w-5/6">
+      <div className="h-auto w-auto text-center float-left">
+        <img className="p-1" src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E" alt="" />
+      </div>
+      <div className="h-auto w-auto text-center float-left">
+        <img className="p-1" src="https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=F7CA3F" alt="" />
+      </div>
+      <div className="h-auto w-auto text-center float-left">
+        <img className="p-1" src="https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white" alt="" />
+      </div>
+      <div className="h-auto w-auto text-center float-left">
+        <img className="p-1" src="https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white" alt="" />
+      </div>
+      <div className="h-auto w-auto text-center float-left">
+        <img className="p-1" src="https://img.shields.io/badge/nestjS-%2320232a.svg?style=for-the-badge&logo=nestjs&logoColor=%23E0234E" alt="" />
+      </div>
+      <div className="h-auto w-auto text-center float-left">
+        <img className="p-1" src="https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB" alt="" />
+      </div>
+      <div className="h-auto w-auto text-center float-left">
+        <img className="p-1" src="https://img.shields.io/badge/flask-%2320232a.svg?style=for-the-badge&logo=flask&logoColor=%23ffffff" alt="" />
+      </div>
+      <div className="h-auto w-auto text-center float-left">
+        <img className="p-1" src="https://img.shields.io/badge/fastapi-%2320232a.svg?style=for-the-badge&logo=FastAPI&logoColor=%019486" alt="" />
+      </div>
+      <div className="h-auto w-auto text-center float-left">
+        <img className="p-1" src="https://img.shields.io/badge/mysql-%23507E9C.svg?style=for-the-badge&logo=mysql&logoColor=white" alt="" />
+      </div>
+      <div className="h-auto w-auto text-center float-left">
+        <img className="p-1" src="https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white" alt="" />
+      </div>
+      <div className="h-auto w-auto text-center float-left">
+        <img className="p-1" src="https://img.shields.io/badge/postgres-%23316192.svg?style=for-the-badge&logo=postgresql&logoColor=white" alt="" />
+      </div>
+      <div className="h-auto w-auto text-center float-left">
+        <img className="p-1" src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB" alt="" />
+      </div>
+      <div className="h-auto w-auto text-center float-left">
+        <img className="p-1" src="https://img.shields.io/badge/nextjs-%23000.svg?style=for-the-badge&logo=next.js&logoColor=white" alt="" />
+      </div>
+      <div className="h-auto w-auto text-center float-left">
+        <img className="p-1" src="https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white" alt="" />
+      </div>
+      <div className="h-auto w-auto text-center float-left">
+        <img className="p-1" src="https://img.shields.io/badge/AWS-%23FF9900.svg?style=for-the-badge&logo=amazon-aws&logoColor=white" alt="" />
+      </div>
+      <div className="h-auto w-auto text-center float-left">
+        <img className="p-1" src="https://img.shields.io/badge/Postman-FF6C37?style=for-the-badge&logo=postman&logoColor=white" alt="" />
+      </div>
+      <div className="h-auto w-auto text-center float-left">
+        <img className="p-1" src="https://img.shields.io/badge/VS%20Code-3EA6E9?style=for-the-badge&logo=visualstudiocode&logoColor=white" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Stacks;
